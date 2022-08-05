ea45931bcf575ea8eb13e71c496afdc92b7efeb0
Lukheri <luke.contrivida@gmail.com>
2022-08-05 17:54:00 +0800

    first commit
    +styling
    +advice api
    +next advice button functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+import desktopDivider from './images/pattern-divider-desktop.svg'
+import mobileDivider from './images/pattern-divider-mobile.svg'
+import diceIcon from './images/icon-dice.svg'
+import axios from 'axios'
 
 function App() {
+  const [advice, setAdvice] = useState({})
+
+  const getAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then(response => setAdvice(response.data.slip))
+  }
+
+  useEffect(getAdvice, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <h5 className='adviceNum'>ADVICE #{advice.id}</h5>
+        <h2 className='adviceText'>"{advice.advice}"</h2>
+        <picture className='divider'>
+            <source media='(min-width: 768px)' srcSet={desktopDivider} />
+            <img src={mobileDivider} alt='' />
+        </picture>
+        <div className='diceButtonContainer'>
+            <button onClick={getAdvice} className='diceButton'>
+                <img src={diceIcon} alt='' />
+            </button>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
